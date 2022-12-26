eab34dad3f202096e721fc7cc26b4391a98eaa41
Rick <rickbatera2012@gmail.com>
2022-12-26 11:08:06 -0300

    Update big-card.component.ts

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,7 +6,8 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit{
-
+  @Input()
+  id:string=""
   @Input()
   tag:string = ""
   @Input()
