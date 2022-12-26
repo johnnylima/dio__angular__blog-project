c8544afb31b692a3b03315e76b4b97204b8fdbad
Rick <rickbatera2012@gmail.com>
2022-12-26 11:08:14 -0300

    Update dataFake.ts

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,36 +7,36 @@ export const dataFake = [
     "preDescription": "Puxa jaguar avestruz codorna um animado querido olá e amarrado e o papa-léguas moral insípido bateu lince longe disso e jeepers riu muito e muito careca aquela píton papa-léguas dentro segurava astutamente o peixe-boi.",
     "description":"uxa jaguar avestruz codorna um animado querido olá e amarrado e o papa-léguas moral insípido bateu lince longe disso e jeepers riu muito e muito careca aquela píton papa-léguas dentro segurava astutamente o peixe-boi. Humano inquieto muito imprudentemente, enquanto lagarta bem, um bem choramingou, acrescentou um, algum sussurrou desenfreadamente, enquanto irritante, longe se agarrou, independentemente, lamentou mais rosadamente e onde saudou enquanto preto querido, então caramba, considerando reformular para algum grosseiro até.",
 
-    "thumb":"thumb-10.jpg"
+    "thumb":"../../assets/thumb-10.jpg"
   },
   {
     "id":"2",
     "tag": "Project",
-    "date": "Dezembro 23, 2022",
+    "date": "Janeiro 27, 2022",
     "title": "9 coisas que adoro em raspar a cabeça durante a quarentena",
     "preDescription": "Sit nemore imperdiet elaboraret eu, commune incidenterint mei no. Ex usu ignota eripuit oportere. Vis at choro oratio, vitae quodsi ei sit. Mei ne habeo legimus qualisque.",
     "description":"uxa jaguar avestruz codorna um animado querido olá e amarrado e o papa-léguas moral insípido bateu lince longe disso e jeepers riu muito e muito careca aquela píton papa-léguas dentro segurava astutamente o peixe-boi. Humano inquieto muito imprudentemente, enquanto lagarta bem, um bem choramingou, acrescentou um, algum sussurrou desenfreadamente, enquanto irritante, longe se agarrou, independentemente, lamentou mais rosadamente e onde saudou enquanto preto querido, então caramba, considerando reformular para algum grosseiro até.",
 
-    "thumb":"thumb-11.jpg"
+    "thumb":"../../assets/thumb-11.jpg"
   },
   {
     "id":"3",
-    "tag": "Life",
-    "date": "Dezembro 23, 2022",
+    "tag": "Audition",
+    "date": "Julho 01, 2022",
     "title": "Qualidades essenciais da música de grande sucesso neste ano",
     "preDescription": "Reque dignissim his in, vix te summo maluisset assueverit, et vidisse luptatum accusata eos.",
     "description":"uxa jaguar avestruz codorna um animado querido olá e amarrado e o papa-léguas moral insípido bateu lince longe disso e jeepers riu muito e muito careca aquela píton papa-léguas dentro segurava astutamente o peixe-boi. Humano inquieto muito imprudentemente, enquanto lagarta bem, um bem choramingou, acrescentou um, algum sussurrou desenfreadamente, enquanto irritante, longe se agarrou, independentemente, lamentou mais rosadamente e onde saudou enquanto preto querido, então caramba, considerando reformular para algum grosseiro até.",
 
-    "thumb":"thumb-12.jpg"
+    "thumb":"../../assets/thumb-12.jpg"
   },
   {
     "id":"4",
-    "tag": "Life",
-    "date": "Dezembro 23, 2022",
+    "tag": "Healthy",
+    "date": "Fevereiro 15, 2022",
     "title": "Por que precisamos parar de falar sobre comida e culpa",
     "preDescription": "Quo ex aeterno luptatum mnesarchum, an duis veri harum eam, mel ex diceret dolorum.",
     "description":"uxa jaguar avestruz codorna um animado querido olá e amarrado e o papa-léguas moral insípido bateu lince longe disso e jeepers riu muito e muito careca aquela píton papa-léguas dentro segurava astutamente o peixe-boi. Humano inquieto muito imprudentemente, enquanto lagarta bem, um bem choramingou, acrescentou um, algum sussurrou desenfreadamente, enquanto irritante, longe se agarrou, independentemente, lamentou mais rosadamente e onde saudou enquanto preto querido, então caramba, considerando reformular para algum grosseiro até.",
 
-    "thumb":"thumb-13.jpg"
+    "thumb":"../../assets/thumb-13.jpg"
   }
 ]
